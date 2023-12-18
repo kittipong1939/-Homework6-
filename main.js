ab73202bdd7d87378bf7ae2eb7a635013c41fa16
kittipong1939 <151816539+kittipong1939@users.noreply.github.com>
2023-12-18 14:22:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+const root = document.querySelector('#root')
+
+function Counter() {
+    function makeElement(tag, attr_n, attr_v, Content) {
+        let output = document.createElement(tag)
+        output.setAttribute(attr_n, attr_v)
+        output.textContent = Content
+        return output
+    }
+    
+    const counter = makeElement('div','class','counter', '')
+    const btnInc = makeElement('button','class','btn-inc','+')
+    const number = makeElement('h3','class','number','0')
+    const btnDec = makeElement('button','class','btn-dec','-')
+    const btnClr = makeElement('button','class','btn-clr','C')
+    const btnDel = makeElement('button','class','btn-del','X')
+    const input = document.getElementById('counterValue')
+
+    let countNum = 0
+    
+    const updateCounter = ()  => {
+        const value = parseInt(input.value || 1) // ถ้า input ไม่มีค่า ใช้ 1 แทน
+        if (!isNaN(value)) {
+            countNum += value
+            number.textContent = countNum
+        }
+    }
+
+    const delCounter = () => {
+        counter.remove()
+    }
+    
+    btnInc.addEventListener('click', updateCounter)
+    btnDec.addEventListener('click', () => {
+        const value = parseInt(input.value || 1) // ถ้า input ไม่มีค่า ใช้ 1 แทน
+        if (!isNaN(value)) {
+            countNum -= value
+            number.textContent = countNum
+        }
+    })
+    
+    btnClr.addEventListener('click', () => {
+        countNum = 0
+        number.textContent = countNum
+    })
+    
+    btnDel.addEventListener('click', delCounter)
+
+    counter.append(btnInc, number, btnDec, btnClr, btnDel)
+    return counter
+}
+
+const hd1AddCounter = () => {
+    root.append(Counter())
+}
+
+const Addbtn = document.querySelector('.addBtn')
+Addbtn.addEventListener('click', hd1AddCounter)
